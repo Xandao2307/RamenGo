7914e658344b66507398887261b06c6260c59f7d
Alexandre Souza <allexandre.souza2307@gmail.com>
2024-05-31 17:54:59 -0300

    Feat: creation broth Service and Communication classes

---
+++ RamenGo.Services/Broth/BrothService.cs
@@ -0,0 +1,41 @@
+﻿using RamenGo.Services.Responses.Exceptions;
+using RamenGo.Communication.Responses;
+using RamenGo.Infrastructure.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RamenGo.Services.Broth
+{
+    public class BrothService
+    {
+        private RamenGoDbContext _dbContext { get; set; }
+
+        public BrothService(RamenGoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<BrothResponse>> GetAllAsync()
+        {
+            var listBroth = new List<BrothResponse>();
+            if (_dbContext != null) throw new BrothException("Error in Database");
+
+            foreach (var broth in _dbContext.Broths)
+                listBroth.Add(new BrothResponse()
+                {
+                    Id = broth.Id,
+                    Description = broth.Description,
+                    ImageActive = broth.ImageActive,
+                    ImageInactive = broth.ImageInactive,
+                    Name = broth.Name,
+                    Price = broth.Price
+                });
+            
+            return listBroth;
+        }
+    }
+}
---
+++ RamenGo.Services/Broth/Exceptions/BrothException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamenGo.Services.Responses.Exceptions
+{
+    public class BrothException : Exception
+    {
+        public BrothException(string? message) : base(message)
+        {
+        }
+    }
+}
