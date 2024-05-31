7283648e2634eea14b90d693ed9dea83ee2a9942
Alexandre Souza <allexandre.souza2307@gmail.com>
2024-05-31 17:56:25 -0300

    Feat: Initiate Order Service

---
+++ RamenGo.Communication/Requests/OrderRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamenGo.Communication.Requests
+{
+    public class OrderRequest
+    {
+        public int BrothId { get; set; }
+        public int ProteinId { get; set; }
+    }
+}
---
+++ RamenGo.Communication/Responses/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamenGo.Communication.Responses
+{
+    public class ErrorResponse
+    {
+        public string ErrorMenssage { get; set; }
+    }
+}
